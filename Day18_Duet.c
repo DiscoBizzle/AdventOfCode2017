3bd566c5368e804bcf60b90edd773e26c88c71df
Jonathan Coulthard <Jon@Jonathans-MacBook-Pro-4.local>
2017-12-19 00:28:46 +0000

    Cleaning up Day 18

--- Day18_Duet.c
+++ Day18_Duet.c
@@ -35,13 +35,17 @@ void PrintProgram(char **Program, int NumInstructions)
 } 
 
 typedef struct _program {
+	int Index;
+	int Status;
+
+	char **Instructions;
 	int InstructionIndex;
-	int ProgramStatus0;
+	int Length;
 
 	int64 *Registers; 
 	int64 *MessageQueue;
 	int NumMessages;
-} Program;
+} program;
 
 void PrintRegisters(int64 *Registers, int NumRegisters, int ProgramIndex)
 {
@@ -52,20 +56,23 @@ void PrintRegisters(int64 *Registers, int NumRegisters, int ProgramIndex)
 	printf("\n");
 }
 
-void ExecuteInstruction(int ProgramIndex, int ProgramSize, char **Program, int64 *Registers, int64 *MessagesReceived, int *NumUnread, 
-							int64 *MessagesSent, int *NumSent, int *TotalSent, int *pInstructionIndex, int *ProgramStatus)
+#define MAXMESSAGES 1000
+#define STATUS_RUNNING 1
+#define STATUS_PAUSED 2
+#define STATUS_DEAD 0
+
+void ExecuteInstructionStruct(program *Program, int64 *MessagesSent, int *NumSent, int *TotalSent)
 {
 	// "Decode" instruction
-	int InstructionIndex = *pInstructionIndex;
-	char *OpCode = Program[3*InstructionIndex];
+	char *OpCode = Program->Instructions[3*Program->InstructionIndex];
 
 	// Arg 2 (if it exists) is always a value.
-	char *Arg2 = Program[3*InstructionIndex+2];
+	char *Arg2 = Program->Instructions[3*Program->InstructionIndex+2];
 	int64 Arg2Value; 
 	int Arg2Exists = 0;
 	if ('a' <= Arg2[0] && Arg2[0] <= 'z') {
 		int RegIndex = Arg2[0] - 'a';
-		Arg2Value = Registers[RegIndex];
+		Arg2Value = Program->Registers[RegIndex];
 		Arg2Exists = 1;
 	} else if (('0' <= Arg2[0] && Arg2[0] <= '9') || (Arg2[0] == '-')) {
 		Arg2Value = atoi(Arg2);
@@ -76,13 +83,13 @@ void ExecuteInstruction(int ProgramIndex, int ProgramSize, char **Program, int64
 
 	// NOTE: Argument 2 will either correspond to a value to use, or a register address to store a value in. We will compute both and 
 	// the instruction decoder will decide which one to use depending on the context. 
-	char *Arg1 = Program[3*InstructionIndex+1];
+	char *Arg1 = Program->Instructions[3*Program->InstructionIndex+1];
 	int Arg1Index = 0; // NOTE: 0 means error. 1 means Register, and we give an index. 2 means Value and we provide the value
 	int64 Arg1Value = 0;
 
 	if ('a' <= Arg1[0] && Arg1[0] <= 'z') {
 		Arg1Index = Arg1[0] - 'a';
-		Arg1Value = Registers[Arg1Index];
+		Arg1Value = Program->Registers[Arg1Index];
 	} else if (('0' <= Arg1[0] && Arg1[0] <= '9') || (Arg1[0] == '-')) {
 		Arg1Value = atoi(Arg1);
 	} else {
@@ -91,64 +98,60 @@ void ExecuteInstruction(int ProgramIndex, int ProgramSize, char **Program, int64
 
 	// Execute instruction
 	if (strcmp(OpCode, "snd") == 0) {
-		printf("Program %d Sending value %lld\n", ProgramIndex, Arg1Value);
 		MessagesSent[*NumSent] = Arg1Value;
 		*NumSent = (*NumSent) + 1;
 		*TotalSent = (*TotalSent) + 1;
+
 	} else if (strcmp(OpCode, "set") == 0) {
 		if (Arg2Exists) {
-			printf("Setting %c = %lld\n", Arg1Index+'a', Arg2Value);
-			Registers[Arg1Index] = Arg2Value;
+			Program->Registers[Arg1Index] = Arg2Value;
 		} else {
 			printf("Error! Invalid 'set' command. Second arguement was not a valid register or an integer\n");
 		}
+
 	} else if (strcmp(OpCode, "add") == 0) {
 		if (Arg2Exists) {
-			printf("Adding %c (%lld) += %lld, result %lld\n", Arg1Index+'a', Arg1Value, Arg2Value, Arg1Value+Arg2Value);
-			Registers[Arg1Index] += Arg2Value;
+			Program->Registers[Arg1Index] += Arg2Value;
 		} else {
 			printf("Error! Invalid 'add' command. Second argument was not a valid register or integer\n");
 		}
+
 	} else if (strcmp(OpCode, "mul") == 0) {
 		if (Arg2Exists) {
-			printf("Multiplying %c (%lld) *= %lld, result %lld\n", Arg1Index+'a', Arg1Value, Arg2Value, Arg1Value*Arg2Value);
-			Registers[Arg1Index] *= Arg2Value;
+			Program->Registers[Arg1Index] *= Arg2Value;
 		} else {
 			printf("Error! Invalid 'mul' command. Second argument was not a valid register or integer\n");
 		}
+
 	} else if (strcmp(OpCode, "mod") == 0) {
 		if (Arg2Exists) {
-			printf("Modulo %c (%lld) %%= %lld, result %lld\n", Arg1Index+'a', Arg1Value, Arg2Value, Arg1Value%Arg2Value);
-			Registers[Arg1Index] %= Arg2Value;
+			Program->Registers[Arg1Index] %= Arg2Value;
 		} else {
 			printf("Error! Invalid 'mod' command. Second argument was not a valid register or integer\n");
 		}
+
 	} else if (strcmp(OpCode, "rcv") == 0) {
-		if ((*NumUnread) > 0) {
-
-			Registers[Arg1Index] = MessagesReceived[0];
-			for (int i = 1; i < *NumUnread; i++) {
-				MessagesReceived[i-1] = MessagesReceived[i];
+		if (Program->NumMessages > 0) {
+			// If we have an unread message, pull it off the queue
+			Program->Registers[Arg1Index] = Program->MessageQueue[0];
+			for (int i = 1; i < Program->NumMessages; i++) {
+				Program->MessageQueue[i-1] = Program->MessageQueue[i];
 			}
-			*NumUnread = (*NumUnread)-1;
-			printf("Program %d Pulled value off queue, %lld. Put it in register %d (%c)\n", ProgramIndex, Registers[Arg1Index], Arg1Index, Arg1Index+'a');
-			// *ProgramStatus = 2;
-
-		} else if ((*NumUnread) == 0) {
-			printf("Program %d Run out of messages. Pausing\n", ProgramIndex);
-			*ProgramStatus = 2;
-			return; // Note. We want to return without incrementing the instructionIndex
+			Program->NumMessages--;
+		} else if (Program->NumMessages == 0) {
+			Program->Status = STATUS_PAUSED;
+			return; // NOTE: We want to return without incrementing the instructionIndex
 		} else {
-			printf("Error! We have a negative number (%d) of unread messages! \n", *NumUnread);
+			printf("Error! We have a negative number (%d) of unread messages! \n", Program->NumMessages);
 		}
-	} else if (strcmp(Program[3*InstructionIndex], "jgz") == 0) {
+
+	} else if (strcmp(OpCode, "jgz") == 0) {
 		if (Arg2Exists) {
 			if (Arg1Value > 0) {
-				printf("Jumping from line %d to %d\n", InstructionIndex, (int32)(InstructionIndex+Arg2Value));
-				InstructionIndex += (Arg2Value - 1); // NOTE: the -1 is required because InstructionIndex will be incremented at the end of the loop.
-				if (InstructionIndex < -1 || InstructionIndex >= (ProgramSize-1)) {
-					printf("InstructionIndex = %d, Terminating Program %d\n", InstructionIndex+1, ProgramIndex);
-					ProgramStatus = 0;
+				Program->InstructionIndex += (Arg2Value - 1); // NOTE: the -1 is required because InstructionIndex will be incremented at the end of the loop.
+				if (Program->InstructionIndex < -1 || Program->InstructionIndex >= (Program->Length-1)) {
+					printf("InstructionIndex = %d, Terminating Program %d\n", Program->InstructionIndex+1, Program->Index);
+					Program->Status = STATUS_DEAD; // Kill the program if we jump outside of the bounds
 					return;
 				}
 			}
@@ -156,11 +159,8 @@ void ExecuteInstruction(int ProgramIndex, int ProgramSize, char **Program, int64
 	} else {
 		printf("Error! Instruction not recognised!\n");
 	}
-	*pInstructionIndex = InstructionIndex + 1;
+	Program->InstructionIndex++;
 }
-
-#define MAXMESSAGES 1000
-
 
 int main(int argc, char const *argv[]) 
 {
@@ -170,59 +170,64 @@ int main(int argc, char const *argv[])
 	if (FilePointer == NULL) { printf("Cannot open file!\n"); return -1; }
 
 	char *WordArray[MAXWORDS];
-	char *Program[MAXWORDS]; 
+	char *Instructions[MAXWORDS]; 
 
 	int InstructionIndex = 0;
 	while ((nread = getline(&Line, &Count, FilePointer)) != -1) {
 		int NumWords = LineToWordArray(WordArray, Line);
 		
-		InsertWordArrayIntoProgram(Program, InstructionIndex, WordArray, NumWords);
+		InsertWordArrayIntoProgram(Instructions, InstructionIndex, WordArray, NumWords);
 		InstructionIndex++;
 	}
-
-	// TODO: Wrap up this shit in data structures
-	// TODO: Learn how multithreading works and actually thread this shit. 
-
 	int NumInstructions = InstructionIndex;
-	int InstructionIndex0 = 0;
-	int InstructionIndex1 = 0;
-	PrintProgram(Program, NumInstructions);
 
-	int ProgramStatus0 = 1;
-	int ProgramStatus1 = 2;
+	// TODO: Bounds checking on the queues. 
+	// TODO: Learn how multithreading works and actually multithread this.
+	program Program0 = {};
 
-	int64 *Registers0 = (int64 *)calloc(26, sizeof(int64)); 
-	int64 *Registers1 = (int64 *)calloc(26, sizeof(int64)); 
-	Registers1['p'-'a'] = 1;
-	int64 SoundFreq = 0; // holds the frequency of the currently playing sound.
-	int64 *MessageQueue0 = (int64 *)calloc(MAXMESSAGES, sizeof(int64));
-	int NumMessages0 = 0;
-	int64 *MessageQueue1 = (int64 *)calloc(MAXMESSAGES, sizeof(int64));
-	int NumMessages1 = 0;
+	Program0.Index = 0;
+	Program0.Status = STATUS_RUNNING;
+	Program0.InstructionIndex = 0;
+	Program0.Length = NumInstructions;
+	Program0.Instructions = Instructions; 
+
+	Program0.Registers = (int64 *)calloc(26, sizeof(int64)); 
+	Program0.MessageQueue = (int64 *)calloc(MAXMESSAGES, sizeof(int64));
+	Program0.NumMessages = 0;
+
+	program Program1 = {};
+
+	Program1.Index = 0;
+	Program1.Status = STATUS_PAUSED;
+	Program1.InstructionIndex = 0;
+	Program1.Length = NumInstructions;
+	Program1.Instructions = Instructions;
+
+	Program1.Registers = (int64 *)calloc(26, sizeof(int64)); 
+	Program1.Registers['p'-'a'] = 1;
+	Program1.MessageQueue = (int64 *)calloc(MAXMESSAGES, sizeof(int64));
+	Program1.NumMessages = 0;
 
 	int TotalSentBy0 = 0;
 	int TotalSentBy1 = 0;
 
 	while (1) {
-		while (ProgramStatus0 == 1) {
+		while (Program0.Status == STATUS_RUNNING) {
 			// NOTE: Program 0 reads from queue 0 and writes to queue 1 and vice-versa
-			ExecuteInstruction(0, NumInstructions, Program, Registers0, MessageQueue0, &NumMessages0,  
-								MessageQueue1, &NumMessages1, &TotalSentBy0, &InstructionIndex0, &ProgramStatus0);
-			PrintRegisters(Registers0, 4, 0);
+			ExecuteInstructionStruct(&Program0, Program1.MessageQueue, &Program1.NumMessages, &TotalSentBy0);
 		}
-		if (ProgramStatus1 == 2 && NumMessages1 > 0) {
-			ProgramStatus1 = 1; 
+		if (Program1.Status == STATUS_PAUSED && Program1.NumMessages > 0) {
+			Program1.Status = STATUS_RUNNING; 
 		}
-		while (ProgramStatus1 == 1) {
-			ExecuteInstruction(1, NumInstructions, Program, Registers1, MessageQueue1, &NumMessages1,  
-								MessageQueue0, &NumMessages0, &TotalSentBy1, &InstructionIndex1, &ProgramStatus1);
-			PrintRegisters(Registers1, 4, 1);
+
+		while (Program1.Status == STATUS_RUNNING) {
+			ExecuteInstructionStruct(&Program1, Program0.MessageQueue, &Program0.NumMessages, &TotalSentBy1);
 		}
-		// printf("MessageQueue0 \n");
-		if (ProgramStatus0 == 2 && NumMessages0 > 0) {
-			ProgramStatus0 = 1;
+		if (Program0.Status == STATUS_PAUSED && Program0.NumMessages > 0) {
+			Program0.Status = STATUS_RUNNING;
 		}
-		if (ProgramStatus0 != 1 && ProgramStatus1 != 1) {
+
+		if (Program0.Status != STATUS_RUNNING && Program1.Status != STATUS_RUNNING) {
 			break;
 		}
 	}
